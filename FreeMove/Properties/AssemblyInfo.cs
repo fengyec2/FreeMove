e583fea7c8d7f6e666b8f69c49bec59a385db4c6
Luminary <85821538+fengyec2@users.noreply.github.com>
2026-04-11 21:58:39 +0800

    build: 更新程序集版本至 3.2.0

--- FreeMove/Properties/AssemblyInfo.cs
+++ FreeMove/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.3")]
-[assembly: AssemblyFileVersion("3.1.3")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
